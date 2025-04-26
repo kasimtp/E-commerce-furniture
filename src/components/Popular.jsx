6c27d821b24ade9bc6318dd205652b4a68c75e10
tpkasimvlr <tpkasimvlr@gmail.com>
2025-04-26 17:05:18 +0530

    new

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,99 +1,42 @@
 import table from "../assets/table.jpg";
 import sofa1 from "../assets/sofa 1.jpg";
 import chair1 from "../assets/chair 1.jpg";
+
 const Popular = () => {
-  const Images = [sofa1, table, chair1];
+  const images = [table, chair1, sofa1, table];
+
   return (
     <div>
-      <div className=" flex flex-col place-content-center gap-12 mt-28 items-center m-auto">
-        <div>
-          {" "}
-          <p className="text-[15px] uppercase  ">Trending now</p>
-        </div>
-        <div>
-          <p className="font-Poppins  font-semibold capitalize text-5xl">
-            Popular This Week
-          </p>
-        </div>
+      <div className="flex flex-col place-content-center gap-12 mt-28 items-center m-auto">
+        <p className="text-[15px] uppercase">Trending now</p>
+        <p className="font-Poppins font-semibold capitalize text-5xl">
+          Popular This Week
+        </p>
       </div>
 
       {/* --------photo section-------------- */}
-      <div className="grid grid-cols-4 mt-24 px-6 gap-8 w-[90%]  m-auto items-center">
-        <div className="flex flex-col gap-8 bg-amber-200 ">
-          <div>
-            <img src={table} alt=""  />
-          </div>
-          <div className="flex flex-col">
+      <div className="grid grid-cols-4 mt-24 px-6 gap-8 w-[90%] m-auto items-center">
+        {images.map((imgSrc, index) => (
+          <div key={index} className="flex flex-col gap-4 p-4 rounded-lg">
             <div>
-              <p>Swing (Copy)</p>
+              <img
+                src={imgSrc}
+                alt={`popular-${index}`}
+                className="h-[350px] w-full object-cover rounded-md"
+              />
             </div>
-            <div>
-              {" "}
-              <p>Swing (Copy)</p>
-            </div>
-            <div>
-              {" "}
-              <p>Swing (Copy)</p>
-            </div>
-          </div>
-        </div>
 
-        <div className="flex flex-col gap-8 ">
-          <div>
-            <img src={chair1} alt="" />
-          </div>
-          <div className="flex flex-col">
-            <div>
-              <p>Swing (Copy)</p>
+            {/* Only text hover/touch animation */}
+            <div className="h-10 overflow-hidden relative group cursor-pointer">
+            <p className="text-lg font-semibold text-center">title</p>
+              <div className="flex flex-col transition-transform duration-500 group-hover:-translate-y-10">
+                <p className="text-lg font-semibold text-center">Relax Chair</p>
+                <p className="text-lg font-semibold text-center">Shipping</p>
+              </div>
             </div>
-            <div>
-              {" "}
-              <p>Swing (Copy)</p>
-            </div>
-            <div>
-              {" "}
-              <p>Swing (Copy)</p>
-            </div>
-          </div>
-        </div>
 
-        <div className="flex flex-col gap-8 ">
-          <div>
-            <img src={sofa1} alt="" />
           </div>
-          <div className="flex flex-col">
-            <div>
-              <p>Swing (Copy)</p>
-            </div>
-            <div>
-              {" "}
-              <p>Swing (Copy)</p>
-            </div>
-            <div>
-              {" "}
-              <p>Swing (Copy)</p>
-            </div>
-          </div>
-        </div>
-
-        <div className="flex flex-col gap-8 ">
-          <div>
-            <img src={table} alt="" />
-          </div>
-          <div className="flex flex-col">
-            <div>
-              <p>Swing (Copy)</p>
-            </div>
-            <div>
-              {" "}
-              <p>Swing (Copy)</p>
-            </div>
-            <div>
-              {" "}
-              <p>Swing (Copy)</p>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
