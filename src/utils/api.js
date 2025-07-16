a02cb481c0e3a186a8766d2cda3f40e69dbde630
Muhammed Kasim TP <tpkasimvlr@gmail.com>
2025-07-16 20:11:16 +0530

    final commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,13 +1,26 @@
-import axios from "axios"
+// import axios from "axios"
 
 
-export const baseUrl = "http://localhost:5000"
+// export const baseUrl = "http://localhost:5000"
 
+
+// export const apiClient = axios.create({
+//     baseURL:baseUrl,
+//     headers:{
+//         "Content-Type":"application/json"
+//     }
+// })
+
+ 
+
+
+import axios from "axios";
+
+export const baseUrl = "https://e-commerce-furniture-backend-gpxh.onrender.com";
 
 export const apiClient = axios.create({
-    baseURL:baseUrl,
-    headers:{
-        "Content-Type":"application/json"
-    }
-})
-
+  baseURL: baseUrl,
+  headers: {
+    "Content-Type": "application/json"
+  }
+});
