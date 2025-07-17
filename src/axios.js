e9be831587877863768c7f487b7cc0f2ae17617e
Muhammed Kasim TP <tpkasimvlr@gmail.com>
2025-07-17 15:29:15 +0530

    final commit

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://e-commerce-furniture-backend-gpxh.onrender.com",
+  baseURL: "https://e-commerce-furniture-backend-gpxh.onrender.com/api", // ✅ include /api
   withCredentials: true,
 });
 
