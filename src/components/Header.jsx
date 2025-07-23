1b0c0acd5a7c5e4c85d3355244232d48c0f2ac20
Muhammed Kasim TP <tpkasimvlr@gmail.com>
2025-07-23 12:03:24 +0530

    final commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -194,7 +194,7 @@ const Header = () => {
           animate="center"
           exit="exit"
           transition={{ duration: 0.8 }}
-          className="absolute w-[300px] h-[180px] sm:h-[350px] md:h-[480px] lg:w-full  object-cover cursor-pointer"
+          className="absolute w-[650px] h-full sm:h-[350px] md:h-[480px] lg:w-full  object-cover cursor-pointer"
           onClick={() => navigate("/shop")}
         />
       </AnimatePresence>
