bb070a1437877d24efaa1e66ee34c641401eb54b
Muhammed Kasim TP <tpkasimvlr@gmail.com>
2025-07-23 14:53:32 +0530

    Your commit message describing the changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -398,20 +398,21 @@ const Header = () => {
       </AnimatePresence>
 
       {/* Optional Dots */}
-      <div className="absolute bottom-2 left-1/2 transform -translate-x-1/2 flex gap-2">
-        {banners.map((_, i) => (
-          <div
-            key={i}
-            onClick={() => {
-              setDirection(i > currentIndex ? 1 : -1);
-              setCurrentIndex(i);
-            }}
-            className={`w-3 h-3 rounded-full ${
-              i === currentIndex ? "bg-black" : "bg-gray-300"
-            } cursor-pointer`}
-          />
-        ))}
-      </div>
+     <div className="absolute bottom-2 sm:bottom-4 md:bottom-6 left-1/2 transform -translate-x-1/2 flex gap-2 z-10">
+  {banners.map((_, i) => (
+    <div
+      key={i}
+      onClick={() => {
+        setDirection(i > currentIndex ? 1 : -1);
+        setCurrentIndex(i);
+      }}
+      className={`w-3 h-3 rounded-full ${
+        i === currentIndex ? "bg-black" : "bg-gray-300"
+      } cursor-pointer`}
+    />
+  ))}
+</div>
+
     </div>
   );
 };
