3740dbacf4dd5ad0c0379d3383f23bcff818cda3
Muhammed Kasim TP <tpkasimvlr@gmail.com>
2025-08-18 17:36:45 +0530

    final commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,3 @@
-// import Banner from "../components/Banner";
-// import Cart from "../components/Cart";
-// import Categories from "../components/Categories";
-// import Footer from "../components/Footer";
-// import Header from "../components/Header";
-// import Popular from "../components/Popular";
-// import Welcome from "../components/Welcome";
-
-
-
-
-// const Home = () => {
-//   return (
-//     <div>
-//       <Header />
-//       <Categories />
-    
-//       <Popular />
-      
-//       <Banner />
-//       <Footer />
-//     </div>
-//   );
-// };
-
-// export default Home;
-
 
 
 
@@ -42,7 +15,7 @@ const Home = () => {
 
   return (
     <div>
-      {/* <Topbanner/> */}
+      {/* <Topbanner /> */}
       <Header />
       <Categories onCategoryClick={setSelectedCategory} /> {/* Pass prop */}
       <Popular selectedCategory={selectedCategory} /> {/* Send to Popular */}
